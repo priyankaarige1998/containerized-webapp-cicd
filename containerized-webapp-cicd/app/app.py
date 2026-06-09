88afe276b08a19266e809c06025268b8bf887f8c
Priyanka <priyankaarige1998@gmail.com>
2026-06-09 13:27:51 +0530

    reset CI pipeline with working tests

--- containerized-webapp-cicd/app/app.py
+++ containerized-webapp-cicd/app/app.py
@@ -1,14 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from DevOps Project!", 200
-
-@app.route("/health")
-def health():
-    return jsonify({"status": "healthy"}), 200
+    return "Hello DevOps!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
