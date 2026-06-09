18afdc3982da9a0af179987c025188bb4e8af0b7
Priyanka <priyankaarige1998@gmail.com>
2026-06-09 09:53:59 +0530

    Fix indentation in test

--- containerized-webapp-cicd/tests/test_app.py
+++ containerized-webapp-cicd/tests/test_app.py
@@ -1,8 +1,7 @@
+ from app.app import app
 
-  from app.app import app
-
-  def test_home():
-     client = app.test_client()
-     response = client.get("/")
-     assert response.status_code == 200
+ def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
 
