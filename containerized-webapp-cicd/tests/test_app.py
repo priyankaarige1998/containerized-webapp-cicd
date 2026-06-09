c83d78686c8ea4f03b3ad325a6c04cde57ca773e
Priyanka <priyankaarige1998@gmail.com>
2026-06-09 09:59:15 +0530

    fix line 2 indentation

--- containerized-webapp-cicd/tests/test_app.py
+++ containerized-webapp-cicd/tests/test_app.py
@@ -1,6 +1,6 @@
- from app.app import app
+from app.app import app
 
- def test_home():
+def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
