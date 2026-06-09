4abfb193f206385363614dfed9f0659ac131fe7c
Priyanka <priyankaarige1998@gmail.com>
2026-06-09 12:29:00 +0530

    fix app and ci pipeline

--- containerized-webapp-cicd/tests/test_app.py
+++ containerized-webapp-cicd/tests/test_app.py
@@ -5,3 +5,8 @@ def test_home():
     response = client.get("/")
     assert response.status_code == 200
 
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.status_code == 200
